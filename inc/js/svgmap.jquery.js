739f02b1989288fb6761048001b9b8a58efb9b8b
Joe Savona <joe@phonogram.co.jp>
2012-01-19 10:51:47 +0900

    svgmap: throw events when the map is ready and for mouse interactions with map areas, allow greater customization of animations applied by default on mouse interactions and allow preventing the default animations through event.preventDefault()

--- inc/js/svgmap.jquery.js
+++ inc/js/svgmap.jquery.js
@@ -9,7 +9,7 @@
     svgmap.defaultOptions = {
         pathAttrs: {
             fill: '#fff', //set from SVG
-            stroke: '#000',
+            stroke: '#000'
         },
         onHoverAttrs: {
             fill: '#ccc'
@@ -24,9 +24,36 @@
         onClickAttrs: {}
     };
 
+    /*
+     *  svgmap.Map($rootNode[, options])
+     *      class for defining an svg 'map' - it is assumed that the SVG represents
+     *      essentially non-overlapping distinct areas (as would be present on a 
+     *      geographical map). provides access to paths by id when an 'id' is set in the
+     *      group (<g>) tag. events are thrown for specific areas on mouse over, mouse out, click.
+     *      all events are thrown at the $rootNode object with event information pointing to the
+     *      id of the area of the target node. also a 'ready' event is thrown once the SVG has been
+     *      retrieved and parsed.
+     *
+     *  params:
+     *      $rootNode: a jQuery wrapped object representing the root node in which the SVG should
+     *                  be displayed. should have a data-mapurl="...url..." attribute specifying
+     *                  the URL of the SVG to be displayed.
+     *      options: (optional). an object literal specifying the default path attributes for displaying areas,
+     *              the attributes to animate to on area hover, and the attributes to animate to when an area
+     *              is clicked. see svgmap.defaultOptions for an example.
+     *
+     *  events:
+     *      type 'svg-ready' @ $rootNode: signifies that the svg has been retrieved successfully and the map
+     *                                      is fully initialized.
+     *      type 'svg-mouseover' @ $rootNode: when an area has been moused over. event.originalEvent.areaId gives
+     *                                      the id of the group the area is in.
+     *      type 'svg-mouseout' @ $rootNode: mouse out version of the above, same way to access the id of the group.
+     *      type 'svg-click' @ $rootNode: mouse click version of the above, same way to access the id of the group.
+     */
     svgmap.Map = function($rootNode, options) {
         var svgUrl;
         this.$rootNode = $rootNode;
+        this.pathsById = {};
         //ensure the top-level keys of the options hash are present
         this.options = _.extend({}, svgmap.emptyOptions, options || {});
         //read in and set defaults for all of the sub-sections of the options
@@ -39,7 +66,7 @@
             $.ajax(svgUrl, {
                 dataType: 'xml',
                 success: _.bind(this.init, this)
-            })
+            });
         }
     };
     svgmap.Map.prototype = {
@@ -48,14 +75,17 @@
                 width,
                 height,
                 pathAttrs,
-                onHoverAttrs,
-                onClickAttrs,
+                mouseOverHandler,
+                mouseOutHandler,
+                clickHandler,
                 areaIndex,
                 areaCount,
                 $area,
                 $svg,
-                $rootNode;
+                $rootNode,
+                pathsById;
             $rootNode = this.$rootNode;
+            pathsById = this.pathsById;
             $svg = $(data).find('svg');
 
             width = parseInt($svg.attr('width'), 10);
@@ -65,24 +95,68 @@
 
             R = Raphael($rootNode.get(0), width, height);
             pathAttrs = this.pathAttrs;
-            onHoverAttrs = this.onHoverAttrs;
-            onClickAttrs = this.onClickAttrs;
+            mouseOverHandler = _.bind(this.mouseOverPath, this);
+            mouseOutHandler = _.bind(this.mouseOutPath, this);
+            clickHandler = _.bind(this.clickPath, this);
 
             $svg.find('path').each(function() {
                 var currentPath;
                 $area = $(this);
                 currentPath = R.path($area.attr('d'));
                 currentPath.attr(pathAttrs);
+                currentPath.areaId = $.trim($area.parents('g').filter('[id]').attr('id'));
+                pathsById[currentPath.areaId] = currentPath;
                 currentPath.mouseover(function() {
-                    currentPath.animate(onHoverAttrs, 500);
+                    var customEvent = $.Event({
+                        type: 'svg-mouseover',
+                        areaId: currentPath.areaId
+                    });
+                    $rootNode.trigger(customEvent);
+                    if (!customEvent.isDefaultPrevented()) {
+                        mouseOverHandler(currentPath);
+                    }
                 });
                 currentPath.mouseout(function() {
-                    currentPath.animate(pathAttrs, 500);
+                    var customEvent = $.Event({
+                        type: 'svg-mouseout',
+                        areaId: currentPath.areaId
+                    });
+                    $rootNode.trigger(customEvent);
+                    if (!customEvent.isDefaultPrevented()) {
+                        mouseOutHandler(currentPath);
+                    }
                 });
                 currentPath.click(function() {
-                    currentPath.animate(onClickAttrs, 500);
+                    var customEvent = $.Event({
+                        type: 'svg-click',
+                        areaId: currentPath.areaId
+                    });
+                    $rootNode.trigger(customEvent);
+                    if (!customEvent.isDefaultPrevented()) {
+                        clickHandler(currentPath);
+                    }
                 });
             });
+            this.$rootNode.trigger($.Event({type: 'svg-ready'}));
+        },
+
+        mouseOverPath: function(path) {
+            path.animate(this.onHoverAttrs, 500);
+        },
+
+        mouseOutPath: function(path) {
+            path.animate(this.pathAttrs, 500);
+        },
+
+        clickPath: function(path) {
+            path.animate(this.onClickAttrs, 500);
+        },
+
+        trigger: function(id, eventType) {
+            var path = this.pathsById[id];
+            if (eventType === 'mouseover') this.mouseOverPath(path)
+            else if (eventType === 'mouseout') this.mouseOutPath(path)
+            else if (eventType === 'click') this.clickPath(path)
         }
     };
 
